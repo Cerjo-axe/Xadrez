e610a81feb5d129881d723d58e7ad456070a9cd5
Sergio Luiz <sl.jr1599@gmail.com>
2020-07-24 22:37:43 -0300

    Tratando erros com exceçao

--- Xadrez_console/Tabuleiro/Tabuleiro.cs
+++ Xadrez_console/Tabuleiro/Tabuleiro.cs
@@ -17,9 +17,38 @@ namespace tabuleiro {
         public Peca ReturnPeca(int linha, int coluna) {
             return Pecas[linha, coluna];
         }
+
+        public Peca ReturnPeca(Posicao pos) {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+
+        public bool ExistePeca(Posicao pos) {
+            ValidarPosicao(pos);
+            return ReturnPeca(pos) != null;
+        }
+
+
+
+
         public void ColocarPeca(Peca p ,Posicao pos) {
+            if (ExistePeca(pos)) {
+                throw new TabuleiroException("Ja existe uma peça nesta posição!!");
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos) {
+            if(pos.Linha<0 || pos.Linha >= Linhas || pos.Coluna<0 || pos.Coluna>=Colunas) {
+                return false;
+            }
+            return true;
+        }
+        public void ValidarPosicao (Posicao pos) {
+            if (!PosicaoValida(pos)) {
+                throw new TabuleiroException("Posição invalida!!");
+            }
         }
     }
 }
